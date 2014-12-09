a6fa9a855adda9f2843bb4d77945381b9590f857
rayab <raq555@abv.bg>
2014-12-09 21:14:31 +0100

    deleted classes

--- com.smarthouse.rest/src/com/smarthouse/rest/status/SmartHouse.java
+++ com.smarthouse.rest/src/com/smarthouse/rest/status/SmartHouse.java
@@ -1,14 +1,11 @@
 package com.smarthouse.rest.status;
 
 public class SmartHouse {
-	public String address = "123 Fake St, Fakeville";
-	private String colour = "blue";
-	
-	 
+	public String address = "Street Name, Number Postcode City Name, Country";
+
     public void setAddress(String title) {
         this.address = title;
     }
-     
     public String getAddress() {
         return address;
     }
